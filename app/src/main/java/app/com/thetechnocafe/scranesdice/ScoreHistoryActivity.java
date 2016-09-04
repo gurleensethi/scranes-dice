3ec9e8d353adf66b9656fde32673ee14dd89a769
gurleensethi <sarusethi@rocketmail.com>
2016-09-05 00:40:54 +0530

    Add recyvler view adapter and view holder

--- app/src/main/java/app/com/thetechnocafe/scranesdice/ScoreHistoryActivity.java
+++ app/src/main/java/app/com/thetechnocafe/scranesdice/ScoreHistoryActivity.java
@@ -3,6 +3,7 @@ package app.com.thetechnocafe.scranesdice;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 public class ScoreHistoryActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +24,16 @@ public class ScoreHistoryActivity extends AppCompatActivity {
     class ViewHolder extends RecyclerView.ViewHolder {
         private TextView mDateText;
         private TextView mUserScoreText;
-        private TextView mScomputerScoreText;
+        private TextView mComputerScoreText;
 
         public ViewHolder(View view) {
             super(view);
-            mDateText = (TextView) findViewById(R.id.date_text);
+            mDateText = (TextView) view.findViewById(R.id.date_text);
+            mUserScoreText = (TextView) view.findViewById(R.id.user_score_history);
+            mComputerScoreText = (TextView) view.findViewById(R.id.computer_score_history);
+        }
+
+        public void bindData() {
 
         }
     }
@@ -39,7 +46,8 @@ public class ScoreHistoryActivity extends AppCompatActivity {
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+            View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.item_score_history, parent, false);
+            return new ViewHolder(view);
         }
 
         @Override
